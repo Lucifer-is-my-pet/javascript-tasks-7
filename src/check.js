599fecbb30fdc7fa9742ffc5f962e087ed98c86d
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-11-15 17:29:50 +0500

    Тестируй меня полностью

--- src/check.js
+++ src/check.js
@@ -1,5 +1,146 @@
 'use strict';
 
 exports.init = function () {
+    Object.prototype.checkContainsKeys = function (keys) {
+        if (typeof keys === 'undefined' || keys.length === 0) {
+            return null;
+        }
+        if (Object.getPrototypeOf(this) === Object.prototype ||
+            Object.getPrototypeOf(this) === Array.prototype) {
+            var contains = true;
+            var thisKeys = Object.keys(this);
+            keys.forEach(function (key) {
+                if (thisKeys.indexOf(key.toString()) === -1) {
+                    contains = false;
+                }
+            });
+            return contains;
+        }
+    };
+
+    Object.prototype.checkHasKeys = function (keys) {
+        if (typeof keys === 'undefined' || keys.length === 0) {
+            return null;
+        }
+        if (Object.getPrototypeOf(this) === Object.prototype ||
+            Object.getPrototypeOf(this) === Array.prototype) {
+            if (Object.keys(this).length !== keys.length) {
+                return false;
+            }
+
+            var stringKeys = keys.map(function (key) {
+                return key.toString();
+            });
+            var contains = true;
+            var thisKeys = Object.keys(this);
+            stringKeys.forEach(function (key) {
+                if (thisKeys.indexOf(key) === -1) {
+                    contains = false;
+                }
+            });
+            thisKeys.forEach(function (key) {
+                if (stringKeys.indexOf(key) === -1) {
+                    contains = false;
+                }
+            });
+            return contains;
+        }
+    };
+
+    Object.prototype.checkContainsValues = function (values) {
+        if (typeof values === 'undefined' || values.length === 0) {
+            return null;
+        }
+        if (Object.getPrototypeOf(this) === Object.prototype ||
+            Object.getPrototypeOf(this) === Array.prototype) {
+            var ownValues = [];
+            for (var i in this) {
+                if (this.hasOwnProperty(i)) {
+                    ownValues.push(this[i]);
+                }
+            }
+            var contains = true;
+            Object.keys(values).forEach(function (key) {
+                if (ownValues.indexOf(values[key]) === -1) {
+                    contains = false;
+                }
+            });
+            return contains;
+        }
+    };
+
+    Object.prototype.checkHasValues = function (values) {
+        if (typeof values === 'undefined' || values.length === 0) {
+            return null;
+        }
+        if (Object.getPrototypeOf(this) === Object.prototype ||
+            Object.getPrototypeOf(this) === Array.prototype) {
+            var ownValues = [];
+            for (var i in this) {
+                if (this.hasOwnProperty(i)) {
+                    ownValues.push(this[i]);
+                }
+            }
+            if (ownValues.length !== values.length) {
+                return false;
+            }
+
+            var contains = true;
+            Object.keys(values).forEach(function (key) {
+                if (ownValues.indexOf(values[key]) === -1) {
+                    contains = false;
+                }
+            });
+            ownValues.forEach(function (value) {
+                if (values.indexOf(value) === -1) {
+                    contains = false;
+                }
+            });
+            return contains;
+        }
+    };
+
+    Object.prototype.checkHasValueType = function (key, type) {
+        if (typeof type === 'undefined' || [String, Number, Function, Array].indexOf(type) < 0) {
+            return null;
+        }
+        if (Object.getPrototypeOf(this) === Object.prototype ||
+            Object.getPrototypeOf(this) === Array.prototype) {
+            // через прототипы не получается, type всегда функция
+            return typeof this[key] === type.name.toLowerCase();
+        }
+    };
+
+    Object.prototype.checkHasLength = function (length) {
+        if (typeof length === 'undefined' || length < 0) {
+            return null;
+        }
+        if (Object.getPrototypeOf(this) === String.prototype ||
+            Object.getPrototypeOf(this) === Array.prototype) {
+            return this.length === length;
+        }
+    };
+
+    Function.prototype.checkHasParamsCount = function (count) {
+        if (typeof count === 'undefined' || count < 0) {
+            return null;
+        }
+        return this.length === count;
+
+    };
+
+    String.prototype.checkHasWordsCount = function (count) {
+        if (typeof count === 'undefined' || count < 0) {
+            return null;
+        }
+        var originalString = this;
+        var splittedString = [];
+        if (this.indexOf('\n') + 1) {
+            splittedString = originalString.split('\n');
+            originalString = splittedString.join(' ');
+        }
+        splittedString = originalString.split(' ');
+        return splittedString.length === count;
+    };
 
 };
