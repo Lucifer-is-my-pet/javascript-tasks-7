5b76e8a8aee496c1a8a62b71c8ddd599ac1daed6
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-11-28 01:17:25 +0500

    'левый карман'.checkContainsKeys('от квартиры');

--- src/check.js
+++ src/check.js
@@ -1,7 +1,13 @@
 'use strict';
 
-function checkIfInvalid(arrayToCheck) {
-    return typeof arrayToCheck === 'undefined' || arrayToCheck.length === 0;
+function checkIfInvalid(dataToCheck) {
+    if (typeof dataToCheck === 'undefined') {
+        return true;
+    }
+    if (Object.getPrototypeOf(dataToCheck) === Array.prototype) {
+        return !dataToCheck.length;
+    }
+    return dataToCheck < 0;
 }
 
 function checkForTypes(objectToCheck, firstType, secondType) {
@@ -14,86 +20,90 @@ exports.init = function () {
         if (checkIfInvalid(keys)) {
             return null;
         }
-        if (checkForTypes(this, Object, Array)) {
-            var contains = true;
-            var thisKeys = Object.keys(this);
-            keys.forEach(function (key) {
-                if (thisKeys.indexOf(key.toString()) === -1) {
-                    contains = false;
-                }
-            });
-            return contains;
+        if (!checkForTypes(this, Object, Array)) {
+            return null;
         }
+        var contains = true;
+        var thisKeys = Object.keys(this);
+        keys.forEach(function (key) {
+            if (thisKeys.indexOf(key.toString()) === -1) {
+                contains = false;
+            }
+        });
+        return contains;
     };
 
     Object.prototype.checkHasKeys = function (keys) {
         if (checkIfInvalid(keys)) {
             return null;
         }
-        if (checkForTypes(this, Object, Array)) {
-            if (Object.keys(this).length !== keys.length) {
-                return false;
-            }
-            var has = this.checkContainsKeys(keys);
-
-            var stringKeys = keys.map(function (key) {
-                return key.toString();
-            });
-            var thisKeys = Object.keys(this);
-            thisKeys.forEach(function (key) {
-                if (stringKeys.indexOf(key) === -1) {
-                    has = false;
-                }
-            });
-            return has;
+        if (!checkForTypes(this, Object, Array)) {
+            return null;
         }
+        var thisKeys = Object.keys(this);
+        if (thisKeys.length !== keys.length) {
+            return false;
+        }
+        var has = this.checkContainsKeys(keys);
+
+        var stringKeys = keys.map(function (key) {
+            return key.toString();
+        });
+        thisKeys.forEach(function (key) {
+            if (stringKeys.indexOf(key) === -1) {
+                has = false;
+            }
+        });
+        return has;
     };
 
     Object.prototype.checkContainsValues = function (values) {
         if (checkIfInvalid(values)) {
             return null;
         }
-        if (checkForTypes(this, Object, Array)) {
-            var ownValues = [];
-            for (var i in this) {
-                if (this.hasOwnProperty(i)) {
-                    ownValues.push(this[i]);
-                }
-            }
-            var contains = true;
-            Object.keys(values).forEach(function (key) {
-                if (ownValues.indexOf(values[key]) === -1) {
-                    contains = false;
-                }
-            });
-            return contains;
+        if (!checkForTypes(this, Object, Array)) {
+            return null;
         }
+        var ownValues = [];
+        for (var i in this) {
+            if (this.hasOwnProperty(i)) {
+                ownValues.push(this[i]);
+            }
+        }
+        var contains = true;
+        Object.keys(values).forEach(function (key) {
+            if (ownValues.indexOf(values[key]) === -1) {
+                contains = false;
+            }
+        });
+        return contains;
     };
 
     Object.prototype.checkHasValues = function (values) {
         if (checkIfInvalid(values)) {
             return null;
         }
-        if (checkForTypes(this, Object, Array)) {
-            var ownValues = [];
-            for (var i in this) {
-                if (this.hasOwnProperty(i)) {
-                    ownValues.push(this[i]);
-                }
-            }
-            if (ownValues.length !== values.length) {
-                return false;
-            }
-
-            var has = this.checkContainsValues(values);
-
-            ownValues.forEach(function (value) {
-                if (values.indexOf(value) === -1) {
-                    has = false;
-                }
-            });
-            return has;
+        if (!checkForTypes(this, Object, Array)) {
+            return null;
         }
+        var ownValues = [];
+        for (var i in this) {
+            if (this.hasOwnProperty(i)) {
+                ownValues.push(this[i]);
+            }
+        }
+        if (ownValues.length !== values.length) {
+            return false;
+        }
+
+        var has = this.checkContainsValues(values);
+
+        ownValues.forEach(function (value) {
+            if (values.indexOf(value) === -1) {
+                has = false;
+            }
+        });
+        return has;
     };
 
     Object.prototype.checkHasValueType = function (key, type) {
@@ -101,19 +111,20 @@ exports.init = function () {
             [String, Number, Function, Array].indexOf(type) < 0) {
             return null;
         }
-        if (checkForTypes(this, Object, Array)) {
-            // через прототипы не получается, type всегда функция
-            return typeof this[key] === type.name.toLowerCase();
+        if (!checkForTypes(this, Object, Array)) {
+            return null;
         }
+        return typeof this[key] === type.name.toLowerCase();
     };
 
     Object.prototype.checkHasLength = function (length) {
         if (checkIfInvalid(length)) {
             return null;
         }
-        if (checkForTypes(this, String, Array)) {
-            return this.length === length;
+        if (!checkForTypes(this, String, Array)) {
+            return null;
         }
+        return this.length === length;
     };
 
     Function.prototype.checkHasParamsCount = function (count) {
@@ -121,21 +132,14 @@ exports.init = function () {
             return null;
         }
         return this.length === count;
-
     };
 
     String.prototype.checkHasWordsCount = function (count) {
         if (checkIfInvalid(count)) {
             return null;
         }
-        var originalString = this;
-        var splittedString = [];
-        if (this.indexOf('\n') + 1) {
-            splittedString = originalString.split('\n');
-            originalString = splittedString.join(' ');
-        }
-        splittedString = originalString.split(' ');
-        return splittedString.length === count;
+        var originalString = this.replace(/\n/g, ' ');
+        return originalString.split(' ').length === count;
     };
 
 };
